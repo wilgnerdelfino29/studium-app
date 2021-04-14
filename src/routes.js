c117bfd8fae6c19a1cf1a23b492a29889e2c6114
wilgnerdelfino29 <wilgnerdelfino@gmail.com>
2021-04-14 16:18:40 -0300

    Adjusting the navigation for multiple stacks and creating the files to do the Preload page

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,13 @@
 import React from 'react';
 import { NavigationContainer} from '@react-navigation/native';
-import { createDrawerNavigator } from "@react-navigation/drawer";
 
-// import CustomDrawer from './components/CustomDrawer';
-import Home from './pages/Home';
-import Menu from './components/Menu';
-import Perfil from './pages/Perfil';
-import Ranking from './pages/Ranking';
-import PostDetail from './pages/PostDetail';
-import Login from './pages/Login';
-
-const Drawer = createDrawerNavigator();
+import MainStack from './stacks/MainStack'
 
 export default function Routes() {
 
     return (
-
         <NavigationContainer>
-            <Drawer.Navigator  initialRouteName="Home">
-                <Drawer.Screen name="Login" component={Login} />
-                <Drawer.Screen name="Home" component={Home} />
-                <Drawer.Screen name="Menu" component={Menu} />
-                <Drawer.Screen name="PostDetail" component={PostDetail}/>
-                <Drawer.Screen name="Perfil" component={Perfil} />
-                <Drawer.Screen name="Ranking" component={Ranking} />
-            </Drawer.Navigator>
+            <MainStack />
         </NavigationContainer>
     );
 }
