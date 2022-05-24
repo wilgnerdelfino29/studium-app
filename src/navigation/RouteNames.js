4788d6343640f73cc691fb64602b78f114e88d78
wilgnerdelfino29 <wilgnerdelfino@gmail.com>
2022-05-24 13:13:52 -0300

    Created RouteNames

---
+++ src/navigation/RouteNames.js
@@ -0,0 +1,12 @@
+export default RouteNames = {
+  PRELOAD: 'Preload',
+  LOGIN: 'Login',
+  SIGNUP: 'SignUp',
+  MAIN_DRAWER: 'MainDrawer',
+  HOME_STACK: 'HomeStack',
+  HOME: 'Home',
+  POST_DETAIL: 'PostDetail',
+  POST_CREATION: 'Criar Post',
+  PERFIL: 'Perfil',
+  RANKING: 'Ranking',
+};
